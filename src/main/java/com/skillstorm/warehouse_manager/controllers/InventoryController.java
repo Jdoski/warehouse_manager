ae9cbe2d50b35c71ae427e8857648624c16eda09
jdoski <jeremydoski3@gmail.com>
2023-07-13 12:29:14 -0400

    cleaned up unused imports

--- src/main/java/com/skillstorm/warehouse_manager/controllers/InventoryController.java
+++ src/main/java/com/skillstorm/warehouse_manager/controllers/InventoryController.java
@@ -10,15 +10,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skillstorm.warehouse_manager.models.Inventory;
-import com.skillstorm.warehouse_manager.models.Item;
-import com.skillstorm.warehouse_manager.models.Warehouse;
 import com.skillstorm.warehouse_manager.repositories.InventoryRepository;
 import com.skillstorm.warehouse_manager.services.InventoryService;
 
--- src/main/java/com/skillstorm/warehouse_manager/services/InventoryService.java
+++ src/main/java/com/skillstorm/warehouse_manager/services/InventoryService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skillstorm.warehouse_manager.models.Inventory;
-import com.skillstorm.warehouse_manager.models.Item;
-import com.skillstorm.warehouse_manager.models.Warehouse;
 import com.skillstorm.warehouse_manager.repositories.InventoryRepository;
 
 @Service
